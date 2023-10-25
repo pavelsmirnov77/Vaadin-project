46e871f303ac83def459baf2d7844a1358d4e59c
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-10-26 01:28:00 +0300

    Реализация базовой структуры проекта

---
+++ vaadin-project/src/main/java/ru/sovkombank/project/VaadinProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.sovkombank.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinProjectApplication.class, args);
+	}
+
+}
